7dae929c2c30c4c6f7303833917f6c61abf96b63
Peterpaine01 <facarlier@gmail.com>
2023-12-27 15:44:41 +0400

    responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,16 @@ import { Link } from "react-router-dom";
 
 // Images
 
+// PDF
+import CV from "../assets/FannyCarlier-CV-DEC2023.pdf";
+
 // Je récupère les props
 const Header = ({ logo }) => {
   return (
     <>
       <header>
         <div className="top-menu">
-          <div className="containe-full flex-parent">
+          <div className="container-full flex-parent">
             <Link className="logo" to="/">
               <p>
                 <strong>Fanny Carlier </strong>- Portfolio
@@ -22,13 +25,39 @@ const Header = ({ logo }) => {
               >
                 Contact
               </Link>
-              <Link className="btn-outlined" to={`/`}>
-                <i class="fa-solid fa-download fa-bounce"></i> Télécharger le cv
-              </Link>
+              <a className="btn-outlined" href={CV} target="_blank">
+                <i className="fa-solid fa-download fa-bounce big-icon"></i>{" "}
+                Télécharger le cv
+              </a>
             </nav>
           </div>
         </div>
       </header>
+
+      {/* burger menu */}
+      <div className="burger-menu">
+        <input
+          className="hamburger"
+          type="checkbox"
+          id="icon-menu-burger"
+          tabindex="0"
+        />
+        <label aria-label="Ouvrir menu" for="icon-menu-burger">
+          <span></span>
+        </label>
+        <nav className="flex-parent menu-mobile">
+          <Link
+            className="btn-light"
+            onClick={() => (window.location = "mailto:facarlier@gmail.com")}
+          >
+            Contact
+          </Link>
+          <a className="btn-outlined" href={CV} target="_blank">
+            <i className="fa-solid fa-download fa-bounce big-icon"></i>{" "}
+            Télécharger le cv
+          </a>
+        </nav>
+      </div>
     </>
   );
 };
